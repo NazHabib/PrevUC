cb1e90ad63308873f9d55eab85467b319987c45d
NazHabib <nazmulhazari9@gmail.com>
2024-05-26 11:01:43 +0100

    requirements fixx

--- website/main/models.py
+++ website/main/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.core.validators import MaxValueValidator, MinValueValidator
-from jsonfield import ListField
+
 
 
 
